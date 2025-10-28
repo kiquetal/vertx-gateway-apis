2c4ae481c1738b59c46643ac57f2908b7d69c1dd
kiquetal <kiquetal@gmail.com>
2025-10-28 11:03:23 -0300

    Rename MainVerticle to Main and update related configurations in pom.xml

--- src/main/java/com/cresterida/gateway/Main.java
+++ src/main/java/com/cresterida/gateway/Main.java
@@ -1,25 +1,25 @@
 package com.cresterida.gateway;
 
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Promise;
+import io.micrometer.prometheusmetrics.PrometheusConfig;
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.http.HttpServerOptions;
 import io.vertx.micrometer.Label;
+import io.vertx.micrometer.MicrometerMetricsFactory;
 import io.vertx.micrometer.MicrometerMetricsOptions;
 import io.vertx.micrometer.VertxPrometheusOptions;
 import io.vertx.micrometer.backends.BackendRegistries;
+import io.vertx.micrometer.backends.BackendRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-public class MainVerticle extends AbstractVerticle
-{
-    private static final Logger logger = LoggerFactory.getLogger(MainVerticle.class);
-
-    @Override
-    public  void start(Promise<Void> startPromise) {
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(String args []) {
         String currentLevel = System.getenv("LOG_LEVEL");
         logger.info("Current LOG_LEVEL is: {}", (currentLevel!= null? currentLevel : "INFO (default)"));
 
@@ -74,14 +74,9 @@ public class MainVerticle extends AbstractVerticle
 
         // Deploy the API Gateway verticle
         vertx.deployVerticle(new ApiGatewayVerticle())
-                .onSuccess(id -> {
-                    logger.info("Gateway started successfully");
-                    startPromise.complete();
-                }
-                )
+                .onSuccess(id -> logger.info("Gateway started successfully"))
                 .onFailure(err -> {
                     logger.error("Failed to start gateway: {}", err.getMessage(), err);
-                    startPromise.fail(err);
                     System.exit(1);
                 });
     }
